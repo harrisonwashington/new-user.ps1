34e603b4bab9c62a5e9dd1464ac3f81ed9f59cd0
harrisonwashington <59775376+harrisonwashington@users.noreply.github.com>
2023-07-17 10:30:01 -0700

    Add files via upload

---
+++ new-user.ps1
@@ -0,0 +1,13 @@
+# Get the user's input for the new user account
+$Username = Read-Host "Enter the new user name:"
+$Password = Read-Host "Enter the new user password:" -AsSecureString
+$FullName = Read-Host "Enter the new user's full name:"
+
+# Create the new user account
+New-LocalUser -Name $Username -Password $Password -FullName $FullName
+
+# Set the user's account expiration date
+Set-LocalUserExpiration -User $Username -ExpirationDate (Get-Date).AddDays(365)
+
+# Add the user to the Administrators group
+Add-LocalGroupMember -Group Administrators -Member $Username
